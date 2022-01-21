4fea7738492e1ded5eda334c681eb893fd87d0d9
Tan Poh Keam <tanpohkeam@tans-air.lan>
2022-01-22 04:52:15 +0800

    reset

---
+++ project-info/about.md
@@ -0,0 +1,31 @@
+
+## Thermal Image Human Action Recongition (TIHAR)
+
+
+### Introduction
+
+Thermal imaging is the process of converting infrared (IR) radiation (heat) into visible images that depict the spatial distribution of temperature differences in a scene viewed by a thermal camera. It is useful in allowing for object distinction without the aid of an illumination source. Thermal imaging is ideal for producing images at night or in the presence of some obscuration, such as light fog, rain, or smoke.
+
+In this project, we investigate the feasibility of using thermal images (through videos) and deep learning techniques to see how it can be used to detect a human object and recognise the human activity that the human object is performing.  We scoped our work to classify three distinct activities - Concentrating, Relaxing, and Exercising. 
+
+
+### What can you do on this site? 
+
+From the drop downlist, you can:
+
+📁 Test the model using sample thermal videos.
+
+🎞️ Upload your own thermal video clip. Please keep to 30 to 40 secs.
+
+🤙 Find out who are the team members.
+
+Fun facts: Tihar refers a very important festival in Asia. You can read more in Wiki.
+https://en.wikipedia.org/wiki/Tihar_(festival)   
+
+
+
+
+
+
+
+
--- streamlit-app-streaming.py
+++ streamlit-app-streaming.py
@@ -16,8 +16,8 @@ VIDEO_EXTENSIONS = ["mp4", "ogv", "m4v", "webm"]
 VIDEO_DIR = "./data"
 UPLOAD_DIR = "./upload"  
 
-PROJECT_INFO = "./project-info/about.md"
-TEAM_DIR = './project-info'
+PROJECT_INFO = "./project-info.md"
+TEAM_DIR = './team'
 
 # Constants for sidebar dropdown
 SIDEBAR_OPTION_PROJECT_INFO = "Show Project Info"
@@ -26,11 +26,17 @@ SIDEBAR_OPTION_UPLOAD_IMAGE = "Upload Your Video"
 SIDEBAR_OPTION_MEET_TEAM = "Meet the Team"
 SIDEBAR_OPTIONS = [SIDEBAR_OPTION_PROJECT_INFO, SIDEBAR_OPTION_DEMO_IMAGE, SIDEBAR_OPTION_UPLOAD_IMAGE, SIDEBAR_OPTION_MEET_TEAM]
 
+# Initialize time
+start = time.time()
+
+
 # Parameters
 clip_frames = 30
-total_frames = 9000  # original 900
+total_frames = 900  # original 900
 inference = True
 frame_count = 0
+
+# st.write("Demo thermal videos files should be stored in  ./data")
 
 def get_video_files_in_dir(directory):
     out = ["< --Select-- >"]
@@ -44,78 +50,24 @@ def get_video_files_in_dir(directory):
                 out.append(item)
     return out
 
-def display_team():
-       st.sidebar.write(" ------ ")
-       stframe_1 = st.empty()
-       stframe_1.subheader("Project Team")
-       stframe_2 = st.empty()
-       first_column, second_column , third_column, forth_column, fifth_column = stframe_2.columns(5)
-       first_column.image("https://via.placeholder.com/300x300?text=Fu%20Wei%20Jiang",  use_column_width = True, caption = "Fu Wei Jiang")
-       second_column.image("https://via.placeholder.com/300x300?text=Jose%20Daniel%20Castor",  use_column_width = True, caption = "Jose Daniel Castor")
-       third_column.image("https://via.placeholder.com/300x300?text=Ching%20Hui%20Lee",  use_column_width = True, caption = "Ching Hui Lee")
-       forth_column.image("https://via.placeholder.com/300x300?text=Eng%20Mong%20Goh",  use_column_width = True, caption = "Eng Mong Goh")
-       fifth_column.image("https://via.placeholder.com/300x300?text=Ming%20Zhang",  use_column_width = True, caption = "Ming Zhang")
-     
-       stframe_2a = st.empty()
-       first_column, second_column , third_column, forth_column, fifth_column = stframe_2a.columns(5)
-       first_column.image(os.path.join( TEAM_DIR, 'tanpohkeam.jpeg'),  use_column_width = True, caption = "Tan Poh Keam")
-       second_column.image(os.path.join( TEAM_DIR, 'loosailam.jpeg'),  use_column_width = True, caption = "Loo Sai Lam")
-       third_column.image("https://via.placeholder.com/300x300?text=Seow%20Khee%20Wei",  use_column_width = True, caption = "Seow Khee Wei")
-      # forth_column.image("https://via.placeholder.com/300x300?text=Jose%20Daniel%20Castor",  use_column_width = True, caption = "Jose Daniel Castor")
-       
-       stframe_3 = st.empty()
-       expandar_contact = stframe_3.expander('Contact Information')
-       expandar_contact.write('Jiang Fu Wei  |   fuwei.jiang@sg.panasonic.com') 
-       expandar_contact.write('Tan Poh Keam  |   tan_poh_keam@rp.edu.sg  |  https://www.linkedin.com/in/tan-pohkeam/')
- 
-def  display_projectinfo():
-        st.sidebar.write(" ------ ")
-        #st.sidebar.success("Project information showing on the right!")
-        with open(PROJECT_INFO, 'r', encoding="utf8") as f:
-           proj_info = f.read()
-           st.write(proj_info)
-
-def run_demo(model):
-       st.sidebar.write("------")
-       files = get_video_files_in_dir(VIDEO_DIR)
-       if len(files) == 1:
-          st.sidebar.write("There are no demo videos avaialble in  ( %s) " % VIDEO_DIR )
-       else:
-          filename = st.sidebar.selectbox("Select a demo video for action detection" , files )
-          if filename != '< --Select-- >':
-          #with st.spinner("Performing Inferfence... this may take a few seconds. Please don't interrupt it."):
-             stframe_1 = st.empty()
-             stframe_1.info("Started")
-             inference_actions(model, os.path.join(VIDEO_DIR, filename) )
-             stframe_1.success("Ended")
-
-def run_upload_demo(model):
-     # This is not an upload with actual file transfer, but to state the path of the video path"
-     #st.write('Placholder: SIDEBAR_OPTION_UPLOAD_IMAGE ')
-     uploaded_file = st.sidebar.file_uploader("Choose a file", type=['mp4'])
-
-     if uploaded_file:
-        #st.write("Filename: ", uploaded_file.name)
-        upload_video_path = os.path.join(UPLOAD_DIR ,uploaded_file.name)
-        with open(upload_video_path,"wb") as f:
-            stframe_1 = st.empty()
-            stframe_1.info("Started")
-            f.write((uploaded_file).getbuffer())
-            inference_actions(model, upload_video_path )
-            stframe_1.success("Ended")
-
 def inference_actions(model, video):
     frame_count = 0
+    # st.video(video)
     cap = cv2.VideoCapture(video)
     frames_list = []
     pred_cls_text = '-'
+    #preds = ['-', '-', '-', '-']  
     preds = ['-', '-', '-']
+
     stframe = st.empty()
 
     while cap.isOpened():
  #   # Obtain the first frame
        success, image = cap.read()  #was .read()
        if not success:
+           # st.write("Ignoring empty camera frame.")
+           # For video, use 'break' instead of 'continue'
+           # continue
            break 
 
        frame_count += 1
@@ -137,9 +89,9 @@ def inference_actions(model, video):
                         fontScale=0.5, color=(0, 255, 255), thickness=2)
              cv2.putText(image, 'Relaxation: {}'.format(preds[2]), org=(500, 160), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=0.5, color=(0, 255, 255), thickness=2)
-
-             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-             stframe.image( image_rgb)
+             #cv2.putText(image, 'Windmill: {}'.format(preds[3]), org=(500, 200), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+             #           fontScale=0.5, color=(0, 255, 255), thickness=2)
+             stframe.image(image)
 
            elif len(frames_list) == clip_frames:
              # continue
@@ -162,6 +114,8 @@ def inference_actions(model, video):
                  pred_cls_text = 'Concentration'
              elif pred_cls == 2:
                  pred_cls_text = 'Relaxation'
+             #elif pred_cls == 3:
+             #   pred_cls_text = 'Windmill'
 
              cv2.putText(image, 'Prediction: {}'.format(pred_cls_text), org=(20, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(0, 255, 255), thickness=2)
@@ -173,10 +127,12 @@ def inference_actions(model, video):
                         fontScale=0.5, color=(0, 255, 255), thickness=2)
              cv2.putText(image, 'Relaxation: {:.2f}'.format(preds[2]), org=(500, 160), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=0.5, color=(0, 255, 255), thickness=2)
-             #cv2.imshow('Inference', image)   # old CV codes
-             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-             stframe.image( image_rgb)
-             
+             #cv2.putText(image, 'Windmill: {:.2f}'.format(preds[3]), org=(500, 200), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+             #           fontScale=0.5, color=(0, 255, 255), thickness=2)
+             #cv2.imshow('Inference', image)
+             stframe.image(image)
+
+
        if not inference:
            stframe.imshow('No Inference', image)
 
@@ -188,26 +144,91 @@ def inference_actions(model, video):
            break
 
     cap.release()
+    # end = time.time()
+    # print('Time elapsed: {:.2f}'.format(end-start))
 
+
+
+###########################
+# Main
+###########################
 def main():
-   # show side bar 
+   ## st.title("What Is The Person Doing?")
    st.sidebar.write(" ------ ")
    st.sidebar.title("Explore the Following")
    app_mode = st.sidebar.selectbox("Please select from the following", SIDEBAR_OPTIONS)
+
    model = tf.keras.models.load_model(MODEL)
 
    if app_mode == SIDEBAR_OPTION_PROJECT_INFO:
-        display_projectinfo()
+        st.sidebar.write(" ------ ")
+        #st.sidebar.success("Project information showing on the right!")
+        with open(PROJECT_INFO, 'r') as f:
+           proj_info = f.read()
+           st.write(proj_info)
+      
 
    elif app_mode ==  SIDEBAR_OPTION_DEMO_IMAGE:
-        run_demo(model)
+       st.sidebar.write("------")
+       files = get_video_files_in_dir(VIDEO_DIR)
+       if len(files) == 1:
+          st.sidebar.write("There are no demo videos avaialble in  ( %s) " % VIDEO_DIR )
+       else:
+          filename = st.sidebar.selectbox("Select a demo video for action detection" , files )
+          if filename != '< --Select-- >':
+          #with st.spinner("Performing Inferfence... this may take a few seconds. Please don't interrupt it."):
+             stframe_1 = st.empty()
+             stframe_1.info("Started")
+             inference_actions(model, os.path.join(VIDEO_DIR, filename) )
+             stframe_1.success("Ended")
             
    elif app_mode ==  SIDEBAR_OPTION_UPLOAD_IMAGE :
-        run_upload_demo(model)
+     # This is not an upload with actual file transfer, but to state the path of the video path"
+     #st.write('Placholder: SIDEBAR_OPTION_UPLOAD_IMAGE ')
+     uploaded_file = st.sidebar.file_uploader("Choose a file", type=['mp4'])
+
+     if uploaded_file:
+        #st.write("Filename: ", uploaded_file.name)
+        upload_video_path = os.path.join(UPLOAD_DIR ,uploaded_file.name)
+        with open(upload_video_path,"wb") as f:
+            stframe_1 = st.empty()
+            stframe_1.info("Started")
+            f.write((uploaded_file).getbuffer())
+            inference_actions(model, upload_video_path )
+            stframe_1.success("Ended")
+
+            #st.write(upload_video_path)
+          
+     
+     # filename = 'sub01_clap_dv_setting02.mp4'
+     
+     #with st.spinner("Performing Inference... this may take a few seconds. Please don't interrupt it."):
+     #   inference_actions(model, upload_video_path )
+     #    Do you wish to try another demo file?")
 
    elif app_mode ==  SIDEBAR_OPTION_MEET_TEAM:   
-       display_team()
+       st.sidebar.write(" ------ ")
+       stframe_1 = st.empty()
+       stframe_1.subheader("Project Team")
 
-
-# run the main program         
+       stframe_2 = st.empty()
+       first_column, second_column , third_column, forth_column, fifth_column = stframe_2.columns(5)
+       first_column.image("https://via.placeholder.com/300x300?text=Fu%20Wei%20Jiang",  use_column_width = True, caption = "Fu Wei Jiang")
+       second_column.image("https://via.placeholder.com/300x300?text=Jose%20Daniel%20Castor",  use_column_width = True, caption = "Jose Daniel Castor")
+       third_column.image("https://via.placeholder.com/300x300?text=Ching%20Hui%20Lee",  use_column_width = True, caption = "Ching Hui Lee")
+       forth_column.image("https://via.placeholder.com/300x300?text=Eng%20Mong%20Goh",  use_column_width = True, caption = "Eng Mong Goh")
+       fifth_column.image("https://via.placeholder.com/300x300?text=Ming%20Zhang",  use_column_width = True, caption = "Ming Zhang")
+     
+       stframe_2a = st.empty()
+       first_column, second_column , third_column, forth_column, fifth_column = stframe_2a.columns(5)
+       first_column.image(os.path.join( TEAM_DIR, 'tanpohkeam.jpeg'),  use_column_width = True, caption = "Tan Poh Keam")
+       second_column.image(os.path.join( TEAM_DIR, 'loosailam.jpeg'),  use_column_width = True, caption = "Loo Sai Lam")
+       third_column.image("https://via.placeholder.com/300x300?text=Seow%20Khee%20Wei",  use_column_width = True, caption = "Seow Khee Wei")
+      # forth_column.image("https://via.placeholder.com/300x300?text=Jose%20Daniel%20Castor",  use_column_width = True, caption = "Jose Daniel Castor")
+       
+       stframe_3 = st.empty()
+       expandar_contact = stframe_3.expander('Contact Information')
+       expandar_contact.write('Jiang Fu Wei  |   fuwei.jiang@sg.panasonic.com') 
+       expandar_contact.write('Tan Poh Keam  |   tan_poh_keam@rp.edu.sg  |  https://www.linkedin.com/in/tan-pohkeam/')
+       
 main()
